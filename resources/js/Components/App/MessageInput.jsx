d0532c21f19c4dbc8335a8ddc326c3424ab72674
webuxmotion <pereverziev.andrii@gmail.com>
2024-05-04 00:40:06 +0300

    3:42:06

---
+++ resources/js/Components/App/MessageInput.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+const MessageInput = ({ conversation = null }) => {
+    const [newMessage, setNewMessage] = useState("");
+    const [inputErrorMessage, setInputErrorMessage] = useState("");
+    const [messageSending, setMessageSending] = useState(false);
+
+    return (
+        <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
+            <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
+
+            </div>
+        </div>
+    );
+}
+
+export default MessageInput;
